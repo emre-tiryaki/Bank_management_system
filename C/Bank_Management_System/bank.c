86363e2fd36dd2ff905ed6eaee1c2fb86a3c172a
Emre Tiryaki <tiryakiemre18@gmail.com>
2023-12-26 17:21:53 +0300

    commit_3
    testing

--- C/Bank_Management_System/bank.c
+++ C/Bank_Management_System/bank.c
@@ -101,8 +101,7 @@ void Main_menu()
     printf("3. Deposit money\n");
     printf("4. Withdraw money\n");
     printf("5. Send money\n");
-    printf("6. Exit");
-    printf("\n");
+    printf("6. Exit\n");
     printf("--------------------------------\n");
     
 }
